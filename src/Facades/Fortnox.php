6894ff0607c520444e76d0d4fa0a5caa080bf594
Erik Bernskiold <erik@bernskioldmedia.com>
2022-04-16 11:38:01 +0200

    accessors

--- src/Facades/Fortnox.php
+++ src/Facades/Fortnox.php
@@ -2,11 +2,21 @@
 
 namespace BernskioldMedia\Fortnox\Facades;
 
+use BernskioldMedia\Fortnox\Resources\AbsenceTransaction;
+use BernskioldMedia\Fortnox\Resources\Account;
+use BernskioldMedia\Fortnox\Resources\AccountChart;
+use BernskioldMedia\Fortnox\Resources\Contract;
+use BernskioldMedia\Fortnox\Resources\FinancialYear;
 use BernskioldMedia\Fortnox\Resources\Invoice;
 use BernskioldMedia\Fortnox\Resources\Sie;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static AbsenceTransaction absenceTransactions()
+ * @method static Account accounts()
+ * @method static AccountChart accountCharts()
+ * @method static Contract contracts()
+ * @method static FinancialYear financialYears()
  * @method static Invoice invoices()
  * @method static Sie sie()
  *
--- src/Fortnox.php
+++ src/Fortnox.php
@@ -2,6 +2,11 @@
 
 namespace BernskioldMedia\Fortnox;
 
+use BernskioldMedia\Fortnox\Resources\AbsenceTransaction;
+use BernskioldMedia\Fortnox\Resources\Account;
+use BernskioldMedia\Fortnox\Resources\AccountChart;
+use BernskioldMedia\Fortnox\Resources\Contract;
+use BernskioldMedia\Fortnox\Resources\FinancialYear;
 use BernskioldMedia\Fortnox\Resources\Invoice;
 use BernskioldMedia\Fortnox\Resources\Sie;
 
@@ -11,6 +16,31 @@ class Fortnox
     public function __construct(
         protected FortnoxClient $client
     ) {
+    }
+
+    public function absenceTransactions(): AbsenceTransaction
+    {
+        return new AbsenceTransaction($this->client);
+    }
+
+    public function accounts(): Account
+    {
+        return new Account($this->client);
+    }
+
+    public function accountCharts(): AccountChart
+    {
+        return new AccountChart($this->client);
+    }
+
+    public function contracts(): Contract
+    {
+        return new Contract($this->client);
+    }
+
+    public function financialYears(): FinancialYear
+    {
+        return new FinancialYear($this->client);
     }
 
     public function invoices(): Invoice

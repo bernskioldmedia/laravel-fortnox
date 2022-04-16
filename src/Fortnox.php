e0adbca617e58aeff8bd8715eb33f1166ec757ef
Erik Bernskiold <erik@bernskioldmedia.com>
2022-04-16 10:31:01 +0200

    Add client

--- src/Fortnox.php
+++ src/Fortnox.php
@@ -2,13 +2,19 @@
 
 namespace BernskioldMedia\Fortnox;
 
+use BernskioldMedia\Fortnox\Resources\Invoice;
+
 class Fortnox
 {
 
     public function __construct(
         protected FortnoxClient $client
-    )
+    ) {
+    }
+
+    public function invoices(): Invoice
     {
+        return new Invoice($this->client);
     }
 
 }

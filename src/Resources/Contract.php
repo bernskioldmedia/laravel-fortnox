14bc470d4eb1b13e088ebd846ac58ffc63bafb97
Erik Bernskiold <erik@bernskioldmedia.com>
2022-04-16 11:34:40 +0200

    Add contracts

---
+++ src/Resources/Contract.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BernskioldMedia\Fortnox\Resources;
+
+use BernskioldMedia\Fortnox\Contracts\Resources\Crud\Createable;
+use BernskioldMedia\Fortnox\Contracts\Resources\Crud\Deleteable;
+use BernskioldMedia\Fortnox\Contracts\Resources\Crud\Readable;
+use BernskioldMedia\Fortnox\Contracts\Resources\Crud\Updateable;
+use BernskioldMedia\Fortnox\Contracts\Resources\Filters\FiltersFilter;
+
+class Contract extends BaseResource
+{
+    use Readable;
+    use Createable;
+    use Updateable;
+    use Deleteable;
+
+    use FiltersFilter;
+
+    public function active(): static
+    {
+        return $this->filter('active');
+    }
+
+    public function inactive(): static
+    {
+        return $this->filter('inactive');
+    }
+
+    public function finished(): static
+    {
+        return $this->filter('finished');
+    }
+
+    public function createInvoice(int $contractId): object
+    {
+        return $this->client->put($this->getEndpoint()."/$contractId/createinvoice")->{"Invoice"};
+    }
+
+    public function increaseInvoiceCount(int $contractId): object
+    {
+        return $this->client->put($this->getEndpoint()."/$contractId/increaseinvoicecount")->{$this->getSingularKey()};
+    }
+
+    protected function getEndpoint(): string
+    {
+        return 'contracts';
+    }
+}
